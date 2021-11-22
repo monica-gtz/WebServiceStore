6e1891e12441c1c36c797d4551c68cd3945dc558
Monica Gutierrez <monica.gtzm@hotmail.com>
2021-11-21 20:34:30 -0600

    fix crud

--- WebServiceStore/Controllers/ClientesController.cs
+++ WebServiceStore/Controllers/ClientesController.cs
@@ -60,17 +60,38 @@ namespace WebServiceStore.Controllers
 
         // PUT api/<ClientesController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> Put(int id, [FromBody] Cliente actualizar)
         {
-            //var c = _db.Cliente
+            using (_db)
+            {
+                var c = await _db.Cliente.SingleOrDefaultAsync(actualizar => actualizar.ClienteId == id);
+                if (c != null)
+                {
+                    c.Nombre = actualizar.Nombre;
+                    c.Telefono = actualizar.Telefono;
+                    c.Mail = actualizar.Mail;
 
-            
+                    await _db.SaveChangesAsync();
+                    
+                }
+                return Ok(actualizar);
+            }            
         }
 
         // DELETE api/<ClientesController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
+            using (_db)
+            {
+                var c = await _db.Cliente.SingleOrDefaultAsync(cliente => cliente.ClienteId == id);
+                if( c != null)
+                {
+                    _db.Cliente.Remove(c);
+                    await _db.SaveChangesAsync();
+                }
+                return Ok(id);
+            }
         }
     }
 }
